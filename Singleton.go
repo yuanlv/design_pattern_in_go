66134b37873172e7466456350552e9a22aea793f
snowfox <yuanlv@126.com>
2014-06-20 22:02:32 +0800

    Update Singleton.go

--- Singleton.go
+++ Singleton.go
@@ -1,8 +1,27 @@
 package main
 
-//import "io"
 import "fmt"
 
+//上帝是一个类
+type God struct {
+	name string
+}
+
+var onlyOneGod *God
+
+//每个人都可以得到我的上帝，但上帝只有一个
+func GetMyGod() *God {
+	if onlyOneGod == nil {
+		onlyOneGod = &God{"I am your God"}
+	}
+	return onlyOneGod
+}
+
+func (god *God) Say() {
+	fmt.Println(god.name)
+}
+
 func main() {
-	fmt.Println("design pattern 1 singleton")
+	fmt.Println("design pattern 1 singleton example code")
+	GetMyGod().Say()
 }
