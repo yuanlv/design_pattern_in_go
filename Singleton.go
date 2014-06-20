f530ed2b00d8723333be2965839a258f3fecf46d
snowfox <yuanlv@126.com>
2014-06-20 21:23:37 +0800

    Update Singleton.go

--- Singleton.go
+++ Singleton.go
@@ -1,10 +1,9 @@
 
 
-    package main
-
-    //import "io"
-    import "fmt"
-
-    func main() {
-	    fmt.Println("design pattern 1 singleton")
-    }
+		package main
+		//import "io"
+		import "fmt"
+		
+		func main() {
+			fmt.Println("design pattern 1 singleton")
+		}
